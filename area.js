5f82faeea1828e6149a2f919e8024d2ec535455c
anujsweb <55160147+anujsweb@users.noreply.github.com>
2021-09-18 14:44:27 +0530

    intial commit
    intial commit

---
+++ area.js
@@ -0,0 +1,16 @@
+const sides = document.querySelectorAll(".side-input");
+const areaBtn = document.querySelector("#area-btn");
+const outputEl = document.querySelector("#output");
+
+function calculateArea(a, b) {
+    const area = a * b / 2;
+    //console.log(sumOfSquares);
+    return area;
+}
+
+function showArea() {
+    const area = calculateArea(Number(sides[0].value), Number(sides[1].value));
+    outputEl.innerText = "Area of triangle is: "+ area;
+}
+
+areaBtn.addEventListener("click", showArea);
